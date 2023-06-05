554ab9f1c025c8445c7c7f09d0ca1bb2933add78
Huseyin Can Ercan <huscanercan@gmail.com>
2023-06-05 09:36:22 +0300

    Add changes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
   @Get()
   listMessages() {
-    return 'list of messages';
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage() {
-    return 'create a message';
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage() {
-    return 'one message';
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
   }
 }
